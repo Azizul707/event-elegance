8d0e40f6214abe929e42807e5dc783b83491473f
Azizul <arafinazizul@gmail.com>
2023-10-08 11:42:48 +0600

    Firebase intregration setup for this project

---
+++ src/Components/Firebase/firebaseConfig.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXQQbh4Z81D0Tb-E-gQh8Wa2YLi9LbAlU",
+  authDomain: "event-elegance-8db17.firebaseapp.com",
+  projectId: "event-elegance-8db17",
+  storageBucket: "event-elegance-8db17.appspot.com",
+  messagingSenderId: "864127174733",
+  appId: "1:864127174733:web:65de4b86e38faebfcb3404"
+};
+
+
+const app = initializeApp( firebaseConfig );
+
+export default app;
